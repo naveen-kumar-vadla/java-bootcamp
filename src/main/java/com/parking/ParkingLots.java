d90c462d1d6cde5ffa4c490738af60c29c99af4b
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-10-04 22:23:41 +0530

    Add ParkingLots.addListenerToAllLots

--- src/main/java/com/parking/ParkingLots.java
+++ src/main/java/com/parking/ParkingLots.java
@@ -1,35 +1,41 @@
 package com.parking;
 
 public class ParkingLots {
-  private final int numberOfLots;
-  private final ParkingLot[] parkingLots;
-  private int currentLotId = 0;
+    private final int numberOfLots;
+    private final ParkingLot[] parkingLots;
+    private int currentLotId = 0;
 
-  public ParkingLots(int numberOfLots, int lotSize) {
-    this.numberOfLots = numberOfLots;
-    this.parkingLots = this.createParkingLots(numberOfLots, lotSize);
-  }
-
-  private ParkingLot[] createParkingLots(int numberOfLots, int lotSize) {
-    ParkingLot[] parkingLots = new ParkingLot[numberOfLots];
-
-    for(int i = 0; i < numberOfLots; i++) {
-      parkingLots[i] = new ParkingLot(lotSize);
-      parkingLots[i].addEventListener(ParkingLotEvent.FULL, this::incCurrentLotId);
+    public ParkingLots(int numberOfLots, int lotSize) {
+        this.numberOfLots = numberOfLots;
+        this.parkingLots = this.createParkingLots(numberOfLots, lotSize);
     }
 
-    return parkingLots;
-  }
+    private ParkingLot[] createParkingLots(int numberOfLots, int lotSize) {
+        ParkingLot[] parkingLots = new ParkingLot[numberOfLots];
 
-  private void incCurrentLotId(ParkingEventInfo status) {
-    this.currentLotId++;
-  }
+        for (int i = 0; i < numberOfLots; i++) {
+            parkingLots[i] = new ParkingLot(lotSize);
+            parkingLots[i].addEventListener(ParkingLotEvent.FULL, this::onParkingLotFull);
+        }
 
-  public ParkingStatus park(Object car) {
-    if(this.currentLotId >= this.numberOfLots) {
-      return ParkingStatus.PARKING_FULL;
+        return parkingLots;
     }
 
-    return this.parkingLots[this.currentLotId].park(car);
-  }
+    private void onParkingLotFull(ParkingEventInfo status) {
+        this.currentLotId++;
+    }
+
+    public ParkingStatus park(Object car) {
+        if (this.currentLotId >= this.numberOfLots) {
+            return ParkingStatus.PARKING_FULL;
+        }
+
+        return this.parkingLots[this.currentLotId].park(car);
+    }
+
+    public void addListenerToAllLots(ParkingLotEvent event, ParkingLotListener parkingLotListener) {
+        for (ParkingLot parkingLot : this.parkingLots) {
+            parkingLot.addEventListener(event, parkingLotListener);
+        }
+    }
 }
--- src/test/java/com/parking/parkingLotsTest.java
+++ src/test/java/com/parking/parkingLotsTest.java
@@ -3,6 +3,7 @@ package com.parking;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class parkingLotsTest {
 
@@ -34,5 +35,29 @@ public class parkingLotsTest {
         parkingLots.park(car1);
 
         assertEquals(ParkingStatus.DONE, parkingLots.park(car2));
+    }
+
+    @Test
+    void shouldInvokeListenersAccordingToEvents() {
+        final ParkingLotListener parkingLotListener1 = mock(ParkingLotListener.class);
+        final ParkingLotListener parkingLotListener2 = mock(ParkingLotListener.class);
+
+        final ParkingLots parkingLots = new ParkingLots(2, 5);
+        final Object car = new Object();
+
+        parkingLots.addListenerToAllLots(ParkingLotEvent.FULL, parkingLotListener1);
+        parkingLots.addListenerToAllLots(ParkingLotEvent.ALMOST_FULL, parkingLotListener2);
+
+        parkingLots.park(car);
+        parkingLots.park(car);
+        parkingLots.park(car);
+        parkingLots.park(car);
+        parkingLots.park(car);
+
+        final ParkingEventInfo expectedParkingEventInfo1 = new ParkingEventInfo(5, 0, ParkingLotEvent.FULL);
+        final ParkingEventInfo expectedParkingEventInfo2 = new ParkingEventInfo(5, 1, ParkingLotEvent.ALMOST_FULL);
+
+        verify(parkingLotListener1).publishEvent(expectedParkingEventInfo1);
+        verify(parkingLotListener2).publishEvent(expectedParkingEventInfo2);
     }
 }
